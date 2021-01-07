640f3e88229cd63d8db02a582b577dc22ecaf15f
GalloJr <renato.gallo@fatec.sp.gov.br>
2021-01-07 16:41:53 -0300

    GET /products

---
+++ backend/src/main/java/com/gallo/dsdeliver/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.gallo.dsdeliver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gallo.dsdeliver.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
